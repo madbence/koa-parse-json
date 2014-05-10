861c295682c1e316986962cbb6a77c9b8b067ab0
Bence Dányi <madbence@gmail.com>
2014-05-10 18:57:04 +0200

    Handle errors

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ module.exports = function(opts) {
   opts = opts || {};
   opts.limit = opts.limit || 2<<20; // 1MB
   return function*(next) {
+    var length = this.req.headers['content-length'];
+    if(length > opts.limit) {
+      this.throw(413);
+    }
     var body = yield* concat(this.req, opts);
+    if(length) {
+      if(body.length < length) {
+        this.throw(400, 'Content-Length mismatch!')
+      } else if(body.length > length) {
+        this.throw(413, 'Content-Length mismatch!')
+      }
+    }
     this.request.body = JSON.parse(body);
     yield* next;
   }
